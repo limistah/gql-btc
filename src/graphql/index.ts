64e4cfe121703a916b1b17fff392735e782b5d02
Aleem Isiaka <aleemisiaka@gmail.com>
2021-05-25 22:21:08 +0100

    Parse the UI to /graphiql url

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -6,9 +6,11 @@ import { Express } from "express";
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  playground: true,
 });
 
 export default (app: Express) => {
   console.log("Applied GraphQL Middleware");
-  apolloServer.applyMiddleware({ app, path: "/graphql" });
+  apolloServer.applyMiddleware({ app, path: "/graphiql" });
 };
